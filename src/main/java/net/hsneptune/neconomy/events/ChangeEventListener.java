b83dac89cc696a10bd4ba03ee9f8b2f5b23518f4
Maneet <110554396+TheManeeter@users.noreply.github.com>
2025-06-29 01:15:04 -0400

    Remove redundant imports

--- src/main/java/net/hsneptune/neconomy/events/ChangeEventListener.java
+++ src/main/java/net/hsneptune/neconomy/events/ChangeEventListener.java
@@ -1,10 +1,6 @@
 package net.hsneptune.neconomy.events;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
-import net.minecraft.block.Blocks;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import net.minecraft.entity.player.PlayerEntity;
+
 public class ChangeEventListener implements ModInitializer {
 
     public static int goldCount;
